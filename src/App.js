7b56486f30355d198f52c0e9e30340c427859d46
Rahul Dudharejiya <rahul2.trinityinfosystem@gmail.com>
2021-09-07 10:27:00 +0530

    raw login page

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './Assets/styles/app.css';
+import Login from './Pages/Login';
+function App() {
+  return <Login/>;
+}
+
+export default hot(module)(App);
