f15da754b78fe85d173ce0214dd054f0b2371017
Rahul Dudharejiya <rahul2.trinityinfosystem@gmail.com>
2021-09-08 16:57:15 +0530

    Sidebar created

--- src/App.js
+++ src/App.js
@@ -1,84 +1,10 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import './Assets/styles/app.css';
-import { Navigation } from '@shopify/polaris';
-import { HomeMajor, CashDollarMajor, QuestionMarkMajor, CircleAlertMajor, SettingsMajor, StoreStatusMajor } from '@shopify/polaris-icons';
-
-//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Sidebar from './Components/Sidebar'
 function App() {
   return (
-    <Navigation location="/">
-    <Navigation.Section
-      items={[
-        {
-          url: '/',
-          label: 'Home',
-          icon: HomeMajor,
-        },
-      ]}
-    />
-    <Navigation.Section
-      title="Statistics"
-      items={[
-        {
-          url: '/statistics/dashboard',
-          disabled: false,
-          label: 'Dashboard',
-        },
-        {
-          url: '/statistics/search_analytics',
-          disabled: false,
-          label: 'Search Analytics',
-        },
-        {
-          url: '/statistics/conversions',
-          label: 'Conversions',
-        },
-      ]}
-    />
-    <Navigation.Section
-    title="Settings"
-    items={[
-      {
-        url: '/settings/instant_search',
-        disabled: false,
-        label: 'Instant search',
-      },
-      {
-        url: '/settings/synonyms',
-        disabled: false,
-        label: 'Synonyms',
-      },
-      {
-        url: '/settings/result_page',
-        label: 'Result page',
-      },
-      {
-        url: '/settings/advanced',
-        label: 'Advanced',
-      },
-    ]}
-    />
-    <Navigation.Section
-    items={[
-        {
-          url: '/pricing',
-          label: 'Pricing',
-          icon: CashDollarMajor,
-        },
-        {
-          url: '/help',
-          label: 'Help',
-          icon: CircleAlertMajor,
-        },
-        {
-          url: '/feature_request',
-          label: 'Feature Request',
-          icon: QuestionMarkMajor,
-        },
-      ]}
-    />
-  </Navigation>
+    <Sidebar/>
   );
 }
 
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,9 +1,95 @@
 import React from "react";
-import { withRouter } from "react-router";
-const Side = () => {
+import { Navigation } from '@shopify/polaris';
+import { HomeMajor, CashDollarMajor, QuestionMarkMajor, CircleAlertMajor, SettingsMajor, StoreStatusMajor,  DropdownMinor } from '@shopify/polaris-icons';
+export default function Sidebar() {
+	const [flag1, setFlag1] = React.useState(false);
+  const [flag2, setFlag2] = React.useState(false);
+  const handleFlag1 = React.useCallback(() => setFlag1(!flag1));
+  const handleFlag2 = React.useCallback(() => setFlag2(!flag2));
 	return (
-		hi
+		<Navigation location="/">
+    <Navigation.Section
+      items={[
+        {
+          url: '/',
+          label: 'Home',
+          icon: HomeMajor,
+        },
+        {
+          url: false,
+          matches: true,
+          label: 'Statistics',
+          icon: StoreStatusMajor,
+          selected: flag1,
+          onClick: handleFlag1,
+          // secondaryAction: {
+          //   icon: DropdownMinor,
+          // },
+          subNavigationItems: [
+            {
+              url: '/statistics/dashboard',
+              disabled: false,
+              label: 'Dashboard',
+            },
+            {
+              url: '/statistics/search_analytics',
+              disabled: false,
+              label: 'Search Analytics',
+            },
+            {
+              url: '/statistics/conversions',
+              label: 'Conversions',
+            },
+          ],
+        },
+        {
+          url: false,
+          matches: true,
+          label: 'Settings',
+          icon: SettingsMajor,
+          selected: flag2,
+          onClick: handleFlag2,
+          // secondaryAction: {
+          //   icon: DropdownMinor,
+          // },
+          subNavigationItems: [
+            {
+              url: '/settings/instant_search',
+              disabled: false,
+              label: 'Instant search',
+            },
+            {
+              url: '/settings/synonyms',
+              disabled: false,
+              label: 'Synonyms',
+            },
+            {
+              url: '/settings/result_page',
+              label: 'Result page',
+            },
+            {
+              url: '/settings/advanced',
+              label: 'Advanced',
+            },
+          ],
+        },
+        {
+          url: '/pricing',
+          label: 'Pricing',
+          icon: CashDollarMajor,
+        },
+        {
+          url: '/help',
+          label: 'Help',
+          icon: CircleAlertMajor,
+        },
+        {
+          url: '/feature_request',
+          label: 'Feature Request',
+          icon: QuestionMarkMajor,
+        },
+      ]}
+    />
+  </Navigation>
 	)	
 };
-const Sidebar = withRouter(Side);
-export default Sidebar
