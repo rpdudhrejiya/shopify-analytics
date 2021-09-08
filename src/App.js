856f9e3921274a3ede624f6977ed7c31e3467c20
Rahul Dudharejiya <rahul2.trinityinfosystem@gmail.com>
2021-09-08 18:35:23 +0530

    row pricing card created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import './Assets/styles/app.css';
-import Sidebar from './Components/Sidebar'
+import Sidebar from './Components/Sidebar';
+import Home from './Pages/Home';
+import { Layout } from '@shopify/polaris';
 function App() {
   return (
-    <Sidebar/>
+    <Layout>
+      <Layout.Section>
+        <Sidebar/>
+      </Layout.Section>
+      <Layout.Section secondary>
+        <Home/>
+      </Layout.Section>
+    </Layout>
   );
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { Card, Heading, Icon, Link, Stack }  from "@shopify/polaris";
+import { CashDollarMajor } from "@shopify/polaris-icons";
 export default function Home() {
 	return (
-		<h2>Hello</h2>
+		<div>
+			<h2>Hello</h2>
+			<Stack>
+				<Link monochrome removeUnderline url="/pricing">
+					<Card title=
+						{
+							<Stack>
+								<Icon source={CashDollarMajor}/>
+								<Heading>Go to pricing</Heading>	
+							</Stack>
+						} sectioned>
+						<p>Check your potential<br/> monthly fee and be<br/> clear about our pricing.</p>
+					</Card>
+				</Link>
+			</Stack>
+		</div>
 	);
 }
